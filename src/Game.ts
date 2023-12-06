bab68b77d23f00f6f71fd1e62a923aad6a1fb825
emiliiog <nocoderstools@gmail.com>
2023-12-06 20:39:17 +0100

    Version desarrollo

--- src/Game.ts
+++ src/Game.ts
@@ -63,7 +63,7 @@ export class Game {
     }
     
     isValidLetter(code: string):boolean {
-        
+
         return  this.#validLetterCodes.includes(code) && this.#actualPosition < MAX_WORD_SIZE;
      }
 
